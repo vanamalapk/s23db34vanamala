36822f841a3e303665719b9b1cae9061fc67b6b6
vanamalapk <123520278+vanamalapk@users.noreply.github.com>
2023-04-19 11:35:22 -0500

    added new files

--- controllers/notebook.js
+++ controllers/notebook.js
@@ -1,26 +1,26 @@
 var notebook = require('../models/notebook');
-// List of all Costumes
+// List of all notebooks
 exports.notebook_list = function(req, res) {
 res.send('NOT IMPLEMENTED: notebook list');
 };
-// for a specific Costume.
+// for a specific notebook.
 // exports.notebook_detail = function(req, res) {
 // res.send('NOT IMPLEMENTED: notebook detail: ' + req.params.id);
 // };
-// Handle Costume create on POST.
+// Handle notebook create on POST.
 exports.notebook_create_post = function(req, res) {
 res.send('NOT IMPLEMENTED: Notebook create POST');
 };
-// Handle Costume delete form on DELETE.
+// Handle notebook delete form on DELETE.
 exports.notebook_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: Notebook delete DELETE ' + req.params.id);
 };
-// Handle Costume update form on PUT.
+// Handle notebook update form on PUT.
 exports.notebook_update_put = function(req, res) {
 res.send('NOT IMPLEMENTED: Notebook update PUT' + req.params.id);
 };
 
-// List of all Costumes
+// List of all notebooks
 exports.notebook_list = async function(req, res) {
     try{
     theNotebook = await notebook.find();
@@ -43,7 +43,7 @@ res.status(500);
 res.send(`{"error": ${err}}`);
 }
 };
-// Handle Costume create on POST.
+// Handle notebook create on POST.
 exports.notebook_create_post = async function(req, res) {
 console.log(req.body)
 let document = new notebook();
@@ -73,7 +73,6 @@ exports.notebook_detail = async function(req, res) {
     };
     
 
-
     // Handle Drink update form on PUT.
 exports.notebook_update_put = async function(req, res) {
  console.log(`update on id ${req.params.id} with body
@@ -100,3 +99,76 @@ ${JSON.stringify(req.body)}`)
 failed`);
  }
 };
+
+
+// Handle notebook delete on DELETE.
+exports.notebook_delete = async function(req, res) {
+  console.log("delete " + req.params.id)
+  try {
+  result = await notebook.findByIdAndDelete( req.params.id)
+  console.log("Removed " + result)
+  res.send(result)
+  } catch (err) {
+  res.status(500)
+  res.send(`{"error": Error deleting ${err}}`);
+  }
+  };
+
+// Handle a show one view with id specified by query
+exports.notebook_view_one_Page = async function(req, res) {
+console.log("single view for id " + req.query.id)
+try{
+result = await notebook.findById( req.query.id)
+res.render('notebookdetail',
+{ title: 'notebook Detail', toShow: result });
+}
+catch(err){
+res.status(500)
+res.send(`{'error': '${err}'}`);
+}
+};
+
+
+// Handle building the view for creating a notebook.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.notebook_create_Page = function(req, res) {
+console.log("create view")
+try{
+res.render('notebookcreate', { title: 'notebook Create'});
+}
+catch(err){
+res.status(500)
+res.send(`{'error': '${err}'}`);
+}
+};
+
+
+// Handle building the view for updating a notebook.
+// query provides the id
+exports.notebook_update_Page = async function(req, res) {
+  console.log("update view for item "+req.query.id)
+  try{
+  let result = await notebook.findById(req.query.id)
+  res.render('notebookupdate', { title: 'notebook Update', toShow: result });
+  }
+  catch(err){
+  res.status(500)
+  res.send(`{'error': '${err}'}`);
+  }
+  };
+
+
+  // Handle a delete one view with id from query
+exports.notebook_delete_Page = async function(req, res) {
+  console.log("Delete view for id " + req.query.id)
+  try{
+  result = await notebook.findById(req.query.id)
+  res.render('notebookdelete', { title: 'notebook Delete', toShow:
+  result });
+  }
+  catch(err){
+  res.status(500)
+  res.send(`{'error': '${err}'}`);
+  }
+  }
--- routes/notebook.js
+++ routes/notebook.js
@@ -5,3 +5,14 @@ var router = express.Router();
 const Notebook_controlers = require('../controllers/notebook');
 router.get('/',Notebook_controlers.notebook_view_all_Page);
 module.exports = router;
+/* GET detail costume page */
+router.get('/detail', Notebook_controlers.notebook_view_one_Page);
+
+/* GET create costume page */
+router.get('/create', Notebook_controlers.notebook_create_Page);
+
+/* GET create update page */
+router.get('/update', Notebook_controlers.notebook_update_Page);
+
+/* GET delete costume page */
+router.get('/delete', Notebook_controlers.notebook_delete_Page);
